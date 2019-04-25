994020e0b8cb6d81a00ed7322bc436e81cd7c781
Asmaathabet <asmathabet2009@hotmail.com>
2019-04-25 12:23:42 +0300

    add navigation links in the main menu and sidebar items.

--- assets/App.vue
+++ assets/App.vue
@@ -1,8 +1,12 @@
 <template>
   <v-app >
-    <v-navigation-drawer fixed color="transparent" v-model="sideNav">
+    <v-navigation-drawer temporary fixed color="transparent" v-model="sideNav">
      <v-list>
-       <v-list-tile v-for="item in menuItems" :key="item.title">
+       <v-list-tile
+        v-for="item in menuItems" 
+        :key="item.title"
+        router 
+        :to="item.link">
          <v-list-tile-action>
            <v-icon>{{ item.icon }}</v-icon>
           </v-list-tile-action>
@@ -15,10 +19,16 @@
       @click.native.stop="sideNav = !sideNav"
       class="hidden-sm-and-up">
       </v-toolbar-side-icon>
-      <v-toolbar-title>Meetup App</v-toolbar-title> 
+      <v-toolbar-title>
+       <router-link to="/" tag="span" style="cursor:pointer"> Meetup App</router-link> 
+       </v-toolbar-title> 
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
-        <v-btn flat v-for="item in menuItems" :key="item.title">
+        <v-btn 
+        flat v-for="item in menuItems" 
+        :key="item.title"
+        router 
+        :to="item.link">
           <v-icon left>{{ item.icon }}</v-icon>
            {{ item.title }}
         </v-btn>
@@ -40,11 +50,11 @@
       return {
        sideNav: false,
        menuItems: [
-          {icon :'supervisor_account' , title :'View Meetups' },
-          {icon :'room' , title :'Organize Meetup' },
-          {icon :'person' , title :'Profile' },
-          {icon :'face' , title :'Sign up' },
-          {icon :'lock_open' , title :'Sign in' }
+          {icon :'supervisor_account' , title :'View Meetups',link:'/meetups' },
+          {icon :'room' , title :'Organize Meetup',link:'/meetups/new' },
+          {icon :'person' , title :'Profile',link:'/profile' },
+          {icon :'face' , title :'Sign up',link:'/signup' },
+          {icon :'lock_open' , title :'Sign in',link:'/signin' }
          ]      
       }
     }
